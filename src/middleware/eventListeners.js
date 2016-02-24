750e684604f0281376be8090e74a53eb729f0c9f
Srinivas Sunkari <srinivas.sunkari@laterooms.com>
2016-02-24 20:49:39 +0000

    Added chat functionality into app

--- src/middleware/eventListeners.js
+++ src/middleware/eventListeners.js
@@ -17,5 +17,28 @@ module.exports = function (io) {
                 numUsers: numUsers
             });
         });
+
+        // when the client emits 'typing', we broadcast it to others
+        socket.on('typing', function () {
+            socket.broadcast.emit('typing', {
+                username: socket.username
+            });
+        });
+
+        // when the client emits 'stop typing', we broadcast it to others
+        socket.on('stop typing', function () {
+            socket.broadcast.emit('stop typing', {
+                username: socket.username
+            });
+        });
+
+        // when the client emits 'new message', this listens and executes
+        socket.on('new message', function (data) {
+            // we tell the client to execute 'new message'
+            socket.broadcast.emit('new message', {
+                username: socket.username,
+                message: data
+            });
+        });
     });
 };
